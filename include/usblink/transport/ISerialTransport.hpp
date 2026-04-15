2bc924ef9776e83c6a2af25af93f9888a40419df
togunchan <togunchan@gmail.com>
2026-04-15 22:46:31 +0300

    Define ISerialTransport interface for serial communication

---
+++ include/usblink/transport/ISerialTransport.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+#include <span>
+
+#include "usblink/transport/SerialConfig.hpp"
+#include "usblink/transport/SerialTypes.hpp"
+
+namespace usblink::transport
+{
+    class ISerialTransport{
+        virtual ~ISerialTransport() = default;
+
+        virtual TransportResult open(const SerialConfig &cfg) = 0;
+        virtual void close() = 0;
+        virtual bool isOpen() const = 0;
+
+        virtual ReadResult read(std::span<std::uint8_t> buffer) = 0;
+        virtual WriteResult write(std::span<const std::uint8_t> data) = 0;
+    };
+} // namespace usblink::transport
