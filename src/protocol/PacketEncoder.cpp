ccfe01376e36c6a3a8a55ff60ae526562d981fed
togunchan <togunchan@gmail.com>
2026-04-23 16:24:37 +0300

    Add CRC32 computation and finalize packet encoding

--- src/protocol/PacketEncoder.cpp
+++ src/protocol/PacketEncoder.cpp
@@ -2,22 +2,63 @@
 
 namespace usblink::protocol
 {
+    uint32_t computeCRC32(std::span<const uint8_t> data)
+    {
+        uint32_t crc = 0xFFFFFFFF;
+
+        for (uint8_t byte : data)
+        {
+            crc ^= byte;
+            for (int i = 0; i < 8; i++)
+            {
+                if (crc & 1)
+                    crc = (crc >> 1) ^ 0xEDB88320;
+                else
+                    crc >>= 1;
+            }
+        }
+
+        return ~crc;
+    }
+
     std::vector<uint8_t> encodePacket(const PacketHeader &header, std::span<const uint8_t> payload)
     {
-        std::vector<uint8_t> buffer;
+        PacketHeader hdr = header;
+        hdr.payloadSize = static_cast<uint32_t>(payload.size());
+        hdr.crc = 0;
 
-        // [HEADER][PAYLOAD]
-        buffer.reserve(sizeof(PacketHeader) + payload.size());
+        std::vector<uint8_t> temp;
+        temp.reserve(sizeof(PacketHeader) + payload.size());
 
         // Treat the header as raw bytes.
         // We're just viewing its memory as uint8_t,
         // not copying it (like memcpy would).
-        const uint8_t *headerPtr = reinterpret_cast<const uint8_t *>(&header);
+        const uint8_t *tempHeaderPtr = reinterpret_cast<const uint8_t *>(&hdr);
+
+        temp.insert(
+            temp.end(),
+            tempHeaderPtr,
+            tempHeaderPtr + sizeof(PacketHeader));
+
+        temp.insert(
+            temp.end(),
+            payload.begin(),
+            payload.end());
+
+        uint32_t crc = computeCRC32(temp);
+
+        hdr.crc = crc;
+
+        std::vector<uint8_t> buffer;
+        // [HEADER][PAYLOAD]
+        buffer.reserve(sizeof(PacketHeader) + payload.size());
+
+        const uint8_t *finalHeaderPtr = reinterpret_cast<const uint8_t *>(&hdr);
 
         buffer.insert(
             buffer.end(),
-            headerPtr,
-            headerPtr + sizeof(PacketHeader));
+            finalHeaderPtr,
+            finalHeaderPtr + sizeof(PacketHeader));
 
         buffer.insert(
             buffer.end(),
